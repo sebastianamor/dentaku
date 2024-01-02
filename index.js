4a3029977b5d75e45fb26ea1d137447ea6ab531b
¨Jane <smoreira11@hotmail.com>
2024-01-02 22:01:11 +0900

    ｃｓｓ出来今からJavaScript

---
+++ index.js
@@ -0,0 +1,4 @@
+const displayvalorAnterior = document.getElementById("valor-anterior");
+const displayvalorActual = document.getElementById("valor-actual");
+const botonesNumeros = document.querySelectorAll(".numero");
+const BotonesOperadores = document.querySelectorAll(".operador");
