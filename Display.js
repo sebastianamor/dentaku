b64476b94187236e05aa9b355b5075dfa993855b
¨Jane <smoreira11@hotmail.com>
2024-01-05 19:19:12 +0900

    まだまだ

--- Display.js
+++ Display.js
@@ -3,12 +3,26 @@ class Display{
         this.displayValorActual = displayValorActual;
         this.displayValorAnterior = displayValorAnterior;
         this.calculador = new Calculadora();
+        this.tipoOperacion = undefined;
         this.valorActual = "";
         this.valorAnterior = "";
     }
 
-    agregarNumero(numero) {
-        this.valorActual = numero;
+    borrar() {
+        this.valorActual = this.valorActual.toString().slice(0,-1);
+        this.imprimirValores();
+    }
+
+    borrarTodo() {
+        this.valorActual = "";
+        this.valorAnterior = "";
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+    }
+
+    agregarNumero(numero) { 
+        if(numero === '.' && this.valorActual.includes('.')) return 
+        this.valorActual = this.valorActual.toString() + numero.toString();
         this.imprimirValores();
     }
 
